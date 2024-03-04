08ca2136978229eddfa95831547fe33335ee74e5
prathmesh <prathmeshjaiswal550@gmail.com>
2024-03-04 14:37:42 +0530

    complete files

---
+++ Conn.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Conn {
+    Connection c;
+    Statement s;
+
+    public Conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc :mysql:///bankmanagementsystem","root","root");
+            s = c.createStatement();
+
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    
+}
